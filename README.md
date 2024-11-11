e6e84bbe2f8a84689bb87a56ec52511ecac2a9bd
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2024-11-11 17:50:05 +0100

    :sparkles: save previously used server address

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Minecraft server
 
-Tools to manage a Minecraft server
+This repository contains two scripts:
+
+- `manage-server.py`: a script to manage a minecraft server
+- `server-status.py`: a script to get information about a server
 
 ## Manage a server
 
-This script support the following features:
+The `manage-server.py` script supports the following features:
 
 - Start a minecraft server
 - Automatically stop/start the server during closing/opening hours
 - Make automatic announcements
+
+### How to use?
 
 ```bash
 python3 manage-server.py
@@ -17,7 +22,11 @@ python3 manage-server.py
 When stopping the script, you will need to manually stop the minecraft server and the screen session.
 You will also need to remove the screenlog.0 file.
 
-## Get a server infos
+## Get a server info
+
+The `server-status.py` script allows you to get information about a server.
+
+### How to use?
 
 ```bash
 # Create a virtual environment
--- server-status.py
+++ server-status.py
@@ -1,13 +1,24 @@
 from mcstatus import JavaServer
 from prompt_toolkit import prompt
-from prompt_toolkit.shortcuts import CompleteStyle
 
-address = input("Enter the server address: ")
+ADDRESS_FILE = "last_address.txt"
+
+try:
+    with open(ADDRESS_FILE, "r") as last_address_file:
+        last_address = last_address_file.read()
+except FileNotFoundError:
+    last_address = ""
+
+address = prompt("Enter the server address: ", default=last_address)
 port = prompt("Enter the server port: ", default="25565")
+
+with open(ADDRESS_FILE, "w") as last_address_file:
+    last_address_file.write(address)
 
 server = JavaServer.lookup(f"{address}:{port}")
 
 status = server.status()
 print(f"The server has {status.players.online} player(s) online and replied in {status.latency} ms")
 if status.players.online > 0:
-    print("Players online (might not be exhaustive):", ", ".join([player.name for player in status.players.sample]))
+    players = ", ".join([player.name for player in status.players.sample])
+    print("Players online (might not be exhaustive):", players)
